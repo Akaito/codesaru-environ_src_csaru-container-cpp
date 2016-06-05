b772dcc2a3ebe4485d9042619eef7f0675387610
Chris Barrett <chris@codesaru.com>
2016-06-04 22:36:04 -0700

    Remove DataMap code. Moved to csaru-datamap-cpp.

--- include/csaru-container-cpp.h
+++ include/csaru-container-cpp.h
@@ -1,7 +1,4 @@
 #pragma once
 
-#include <csaru-container-cpp/DataNode.hpp>
-#include <csaru-container-cpp/DataMap.hpp>
-#include <csaru-container-cpp/DataMapReader.hpp>
-#include <csaru-container-cpp/DataMapMutator.hpp>
-#include <csaru-container-cpp/DataMapReaderSimple.hpp>
+#include <csaru-container-cpp/ObjectPool.hpp>
+
